ec931a6f9fa1c2dc825578635e2da8f670f50742
Amogh Mishra <53436778+amoghmishra15@users.noreply.github.com>
2021-12-03 15:56:33 +0530

    First experiment
    1. Loading the image
    2. Black and White with different thresholds
    3. Grayscale
    4. Histograms and manipulation

---
+++ DIP_1.m
@@ -0,0 +1,51 @@
+clc; clear; close all;
+
+% Load
+
+orig = imread('Group.jpg');
+subplot(3,3,1);
+imshow(orig);
+title('Original');
+
+% Logical
+
+bw = im2bw(orig);
+subplot(3,3,2);
+imshow(bw);
+title('Logical at 100% threshold');
+
+% Logical - 90%
+
+bw1 = im2bw(orig, 0.9);
+subplot(3,3,3);
+imshow(bw1);
+title('Logical at 90% threshold');
+
+% Grayscale
+gray = rgb2gray(orig);
+subplot(3,3,4);
+imshow(gray);
+title('Grayscale');
+
+% Histogram
+subplot(3,3,5);
+imhist(gray);
+title('Histogram of grayscale');
+
+% Histogram sliding
+gSlide = gray-100;
+subplot(3,3,6);
+imhist(gSlide);
+title('Histogram after sliding by 100');
+subplot(3,3,7);
+imshow(gSlide);
+title('Grayscale after sliding by 100');
+
+% Histogram equalization
+[eq,T] = histeq(gray);
+subplot(3,3,8);
+imhist(orig);
+title('Histogram after equalization');
+subplot(3,3,9);
+imshow(orig);
+title('Image after equalization');
